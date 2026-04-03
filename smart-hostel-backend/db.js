68407aff95e3dd3d1cd462976ef56947487f0184
Hrishabh24 <darkfusion264@gmail.com>
2026-04-04 00:02:21 +0530

    Fixed Railway DB connection

--- smart-hostel-backend/db.js
+++ smart-hostel-backend/db.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Vani",
-  database: "smart_hostel"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
